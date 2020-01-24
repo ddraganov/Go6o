8e6ce12053ae10a816b25f55c28538e55377cbea
Dragomir <dragomird@directferries.com>
2020-01-24 14:34:06 +0200

    i like boobs

---
+++ src/Go6o.Api/DTOs/SuccessFailTestCreationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Go6o.Api.DTOs
+{
+    public class SuccessFailTestCreationRequest : TestCreationRequestBase
+    {
+        public int HitsCountRequiredForRecalculation { get; set; }
+    }
+}
