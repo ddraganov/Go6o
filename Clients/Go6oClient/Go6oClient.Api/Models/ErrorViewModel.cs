22d62e538c11391ff84c86e8d84f99c343fb3f82
Stoyko Neykov <stoykon@directferries.com>
2020-01-24 10:59:40 +0200

    Scafold client

---
+++ Clients/Go6oClient/Go6oClient.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Go6oClient.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
