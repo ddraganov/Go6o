ab05c29692c39a67d9977a68fb25b5728997a9f8
Hristo Stefanov <hristos@directferries.com>
2020-01-24 09:28:07 +0200

    Implemented basic queue processor.

--- src/Go6o.QueueProcessor/Program.cs
+++ src/Go6o.QueueProcessor/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.SQS;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,8 +19,21 @@ namespace Go6o.QueueProcessor
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                            .Build();
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
+                    // AWS Configuration
+                    var awsOptions = hostContext.Configuration.GetAWSOptions();
+                    services.AddDefaultAWSOptions(awsOptions);
+                    services.AddAWSService<IAmazonSQS>();
+
                     services.AddHostedService<Worker>();
                 });
     }
--- src/Go6o.QueueProcessor/Worker.cs
+++ src/Go6o.QueueProcessor/Worker.cs
@@ -1,28 +1,56 @@
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Go6o.QueueProcessor
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IAmazonSQS _sqs;
 
-        public Worker(ILogger<Worker> logger)
+        private readonly string _eventQueueUrl = "https://sqs.eu-west-1.amazonaws.com/777530757256/abtesting-queue";
+
+        public Worker(ILogger<Worker> logger, IAmazonSQS sqs)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _sqs = sqs ?? throw new ArgumentNullException(nameof(sqs));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                try
+                {
+                    var request = new ReceiveMessageRequest
+                    {
+                        QueueUrl = _eventQueueUrl,
+                        MaxNumberOfMessages = 10,
+                        WaitTimeSeconds = 5
+                    };
+
+                    var result = await _sqs.ReceiveMessageAsync(request);
+                    if(result.Messages.Any())
+                    {
+                        foreach (var message in result.Messages)
+                        {
+                            // Some Processing code would live here
+                            _logger.LogInformation("Processing Message: {message} | {time}", message.Body, DateTimeOffset.Now);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.InnerException.ToString());
+                }
+
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
             }
         }
     }
